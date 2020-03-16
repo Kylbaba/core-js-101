720f226278fe1718fdfed561f0748649a02088f2
Kylbaba <petrenkotanya18@gmail.com>
2020-03-16 22:51:01 +0200

    complete 1st task

---
+++ tasks/reversed-strings.js
@@ -0,0 +1,17 @@
+/**
+ * Complete the solution so that it reverses the string value passed into it.
+ * https://www.codewars.com/kata/reversed-strings
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ */
+function solution(str){
+  let newString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+solution('world'); // returns 'dlrow'
